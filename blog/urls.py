75c990c1f7f88762cd50192ae3f9f0bbadbee1f8
daramariabs <darasousa09@gmail.com>
2021-12-05 15:37:16 -0300

    views e urls

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+#Aqui esta sendo importado a funçao url e todas as views do app blog.
+from django import urls
+from django.conf.urls import url
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
